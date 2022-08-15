75b4df5a4ab5596cf52fa001b77eb38e7ce00e94
parasharparth <parthparashar6@gmail.com>
2022-08-15 03:51:04 -0700

    Checking if vowel present in a string or not

---
+++ src/practice/august15/string/VowelCheck.java
@@ -0,0 +1,20 @@
+package practice.august15.string;
+
+import java.util.Scanner;
+
+public class VowelCheck {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        sc.close();
+        if(input.matches(".*[aeiouAEIOU].*"))
+        {
+            System.out.println("Yes, Present");
+        }
+        else {
+            System.out.println("Not Present");
+        }
+    }
+}
