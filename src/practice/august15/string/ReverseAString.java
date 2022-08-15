ce9945e6244acaeba42d94d834514e45bd5b2b24
parasharparth <parthparashar6@gmail.com>
2022-08-15 03:50:32 -0700

    Reversing a string

---
+++ src/practice/august15/string/ReverseAString.java
@@ -0,0 +1,19 @@
+package practice.august15.string;
+
+import java.util.Scanner;
+
+public class ReverseAString {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        char[] input = sc.next().toCharArray();
+        sc.close();
+        StringBuilder builder = new StringBuilder();
+        for(int i = input.length-1; i>=0 ;i--)
+        {
+            builder.append(input[i]);
+        }
+        System.out.println(builder.toString());
+    }
+}
