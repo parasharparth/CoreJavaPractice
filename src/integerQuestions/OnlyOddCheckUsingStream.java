3069b9341996064e5cdaa333c68b1ff7a4fad16f
parasharparth <parthparashar6@gmail.com>
2022-08-15 17:43:33 -0700

    Only Odd checker

---
+++ src/integerQuestions/OnlyOddCheckUsingStream.java
@@ -0,0 +1,33 @@
+package integerQuestions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class OnlyOddCheckUsingStream {
+
+    public static void main(String[] args)
+    {
+        List<Integer> list = new ArrayList<>();
+        System.out.println("Enter 4 numbers: -");
+        Scanner sc = new Scanner(System.in);
+        list.add(sc.nextInt());
+        list.add(sc.nextInt());
+        list.add(sc.nextInt());
+        list.add(sc.nextInt());
+        sc.close();
+        boolean result = onlyOddPresentOrNot(list);
+        if(result)
+        {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean onlyOddPresentOrNot(List<Integer> list)
+    {
+        return list.parallelStream().anyMatch(x->x%2 != 0);
+    }
+}
