c347380f65c6d8bab23567a117fb445e395bbcac
parasharparth <parthparashar6@gmail.com>
2022-08-15 23:49:58 -0700

    checking only odd in list

---
+++ src/practice/august15b/integerQuestions/OnlyOddCheckUsingStream.java
@@ -0,0 +1,35 @@
+package practice.august15b.integerQuestions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class OnlyOddCheckUsingStream {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter 4 elements of the list");
+        List<Integer> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        list.add(sc.nextInt());
+        list.add(sc.nextInt());
+        list.add(sc.nextInt());
+        list.add(sc.nextInt());
+        sc.close();
+        boolean result = check(list);
+        if(result)
+        {
+            System.out.println("Only odd numbers present");
+        }
+        else
+        {
+            System.out.println("Some even numbers also present");
+        }
+    }
+
+    public static boolean check(List<Integer> list)
+    {
+        return list.parallelStream().anyMatch(x->x%2 != 0);
+    }
+
+}
