3f4d48f029b85b36b389b9a7b4af902b5f97e63d
parasharparth <parthparashar6@gmail.com>
2022-08-15 23:50:07 -0700

    checking only odd in list

---
+++ src/practice/august15b/integerQuestions/OnlyOddPresentOrNot.java
@@ -0,0 +1,19 @@
+package practice.august15b.integerQuestions;
+
+import java.util.*;
+
+public class OnlyOddPresentOrNot {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter 4 elements");
+        List<Integer> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        list.add(sc.nextInt());
+        list.add(sc.nextInt());
+        list.add(sc.nextInt());
+        list.add(sc.nextInt());
+        sc.close();
+
+    }
+}
