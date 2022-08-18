a36e8060113a2897cf75728febaa6f48df64e32f
parasharparth <parthparashar6@gmail.com>
2022-08-18 04:13:46 -0700

    Only Odd Present

---
+++ src/practice/august17/integerQuestions/OnlyOddPresentOrNot.java
@@ -0,0 +1,27 @@
+package practice.august17.integerQuestions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class OnlyOddPresentOrNot {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter 4 elements");
+        List<Integer> list = new ArrayList<>();
+        list.add(new Scanner(System.in).nextInt());
+        list.add(new Scanner(System.in).nextInt());
+        list.add(new Scanner(System.in).nextInt());
+        list.add(new Scanner(System.in).nextInt());
+        for(int i=0; i<list.size(); i++)
+        {
+            if(list.get(i) % 2 == 0)
+            {
+                System.out.println("No, Even numbers are present");
+                System.exit(0);
+            }
+        }
+        System.out.println("Yes, Only odd numbers are present");
+    }
+}
