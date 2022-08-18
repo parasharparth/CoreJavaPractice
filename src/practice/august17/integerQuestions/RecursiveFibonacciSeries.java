60a63383a69157f564d44890a69380028fed5e25
parasharparth <parthparashar6@gmail.com>
2022-08-18 04:14:30 -0700

    RecursiveFibonacciSeries.java - 2

---
+++ src/practice/august17/integerQuestions/RecursiveFibonacciSeries.java
@@ -0,0 +1,27 @@
+package practice.august17.integerQuestions;
+
+import java.util.Scanner;
+
+public class RecursiveFibonacciSeries {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the Number of elements");
+        int n = new Scanner(System.in).nextInt();
+        for(int i=0; i<n; i++)
+        {
+            System.out.println(fibonacci(i));
+        }
+    }
+
+    public static int fibonacci(int n)
+    {
+        if(n == 1 || n == 0)
+        {
+            return n;
+        }
+        else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+}
