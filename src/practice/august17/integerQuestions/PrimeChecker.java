193e0cdee64e2594d91a4552022e28e9c9793045
parasharparth <parthparashar6@gmail.com>
2022-08-18 04:14:03 -0700

    Prime Checker

---
+++ src/practice/august17/integerQuestions/PrimeChecker.java
@@ -0,0 +1,37 @@
+package practice.august17.integerQuestions;
+
+import java.util.Scanner;
+
+public class PrimeChecker {
+
+    public static void main(String[] args)
+    {
+        if(prime(new Scanner(System.in).nextInt()))
+        {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean prime(int n)
+    {
+        if(n == 0 || n == 1)
+        {
+            return false;
+        }
+        if(n == 2)
+        {
+            return true;
+        }
+        for(int i=2 ; i<=n/2; i++)
+        {
+            if(n%i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
