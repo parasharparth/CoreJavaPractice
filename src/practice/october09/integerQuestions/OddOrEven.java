6e1b59ea25ee45abc86dbd04724f2b650779e574
parasharparth <parthparashar6@gmail.com>
2022-10-09 23:35:58 -0700

    October09-odd-or-even-1

---
+++ src/practice/october09/integerQuestions/OddOrEven.java
@@ -0,0 +1,20 @@
+package practice.october09.integerQuestions;
+
+public class OddOrEven {
+
+    public static boolean oddOrEven(int element)
+    {
+        if(element == 0 || element == 0)
+        {
+            return false;
+        }
+        else if(element %2 ==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
