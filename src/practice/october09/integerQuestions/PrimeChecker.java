91b840113ddb97c25ee2b042d093b3b291cf4797
parasharparth <parthparashar6@gmail.com>
2022-10-09 23:36:11 -0700

    October09-prime-checker

---
+++ src/practice/october09/integerQuestions/PrimeChecker.java
@@ -0,0 +1,16 @@
+package practice.october09.integerQuestions;
+
+public class PrimeChecker {
+
+    public static boolean primeOrNot(int num)
+    {
+        if(num == 1 || num == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
