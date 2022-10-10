92697d9ff292b4be84d3bba212b1fe7b28b5bfaf
parasharparth <parthparashar6@gmail.com>
2022-10-09 23:35:47 -0700

    October09-odd-or-even

---
+++ src/practice/october04/integerQuestions/OddOrEven.java
@@ -0,0 +1,13 @@
+package practice.october04.integerQuestions;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the number you want to check");
+        int a = new Scanner(System.in).nextInt();
+
+    }
+}
