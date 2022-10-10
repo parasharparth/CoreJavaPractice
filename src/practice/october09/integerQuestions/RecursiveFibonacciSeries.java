600c26e7932e2f8dde0f08abeab4ab8066a6e18e
parasharparth <parthparashar6@gmail.com>
2022-10-09 23:36:26 -0700

    October09-recursive-fibonacci-series

---
+++ src/practice/october09/integerQuestions/RecursiveFibonacciSeries.java
@@ -0,0 +1,26 @@
+package practice.october09.integerQuestions;
+
+public class RecursiveFibonacciSeries {
+
+    public static void fib(int num)
+    {
+        for(int i=0; i<num; i++)
+        {
+            System.out.println(fibonacci(i)+" ");
+        }
+    }
+
+    public static int fibonacci(int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        if(n == 1 || n == 2)
+        {
+            return 1;
+        }
+        return fibonacci(n-2) + fibonacci(n-1);
+
+    }
+}
