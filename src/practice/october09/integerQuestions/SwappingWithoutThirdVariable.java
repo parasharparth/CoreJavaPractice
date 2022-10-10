ab740e7b1afcd9ad8c64b013d46be3e12999618b
parasharparth <parthparashar6@gmail.com>
2022-10-09 23:36:39 -0700

    October09-swapping-third-variable

---
+++ src/practice/october09/integerQuestions/SwappingWithoutThirdVariable.java
@@ -0,0 +1,14 @@
+package practice.october09.integerQuestions;
+
+public class SwappingWithoutThirdVariable {
+
+    public static void swap(int a, int b)
+    {
+        b = b + a;
+        a = b - a;
+        b = b - a;
+
+        System.out.println("a:- "+a);
+        System.out.println("b:- "+b);
+    }
+}
