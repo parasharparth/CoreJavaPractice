8bf114c7d4347ba8ae9e1c22ba76c618687ff1ab
parasharparth <parthparashar6@gmail.com>
2022-10-21 03:40:19 -0700

    October21-main-class

---
+++ src/practice/october21/driver/MainClass.java
@@ -0,0 +1,13 @@
+package practice.october21.driver;
+
+import practice.october21.integerQuestions.SwappingWithoutThird;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+    public static void main(String[] args)
+    {
+        SwappingWithoutThird.swap(new Scanner(System.in).nextInt(), new Scanner(System.in).nextInt());
+    }
+}
