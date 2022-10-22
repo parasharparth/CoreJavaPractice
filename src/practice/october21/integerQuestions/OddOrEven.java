4de7f6d5b76952190eff4d3491bd761169119170
parasharparth <parthparashar6@gmail.com>
2022-10-22 00:58:27 -0700

    October21-odd-even

---
+++ src/practice/october21/integerQuestions/OddOrEven.java
@@ -0,0 +1,21 @@
+package practice.october21.integerQuestions;
+
+public class OddOrEven {
+
+    public static void oddOrEven(int num)
+    {
+        if(num == 0 || num == 1)
+        {
+            System.out.println("Neither Odd Nor Even");
+            return;
+        }
+        if(num%2 == 0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+}
