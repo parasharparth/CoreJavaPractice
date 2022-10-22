1a5e5c5b8a12473b93292b5ac197d94efb22614c
parasharparth <parthparashar6@gmail.com>
2022-10-22 00:57:59 -0700

    October21-main-class

--- src/practice/october21/driver/MainClass.java
+++ src/practice/october21/driver/MainClass.java
@@ -1,5 +1,8 @@
 package practice.october21.driver;
 
+import practice.october21.integerQuestions.FibonacciSeries;
+import practice.october21.integerQuestions.OddOrEven;
+import practice.october21.integerQuestions.PrimeChecker;
 import practice.october21.integerQuestions.SwappingWithoutThird;
 
 import java.util.Scanner;
@@ -9,5 +12,8 @@ public class MainClass {
     public static void main(String[] args)
     {
         SwappingWithoutThird.swap(new Scanner(System.in).nextInt(), new Scanner(System.in).nextInt());
+        FibonacciSeries.fib(new Scanner(System.in).nextInt());
+        OddOrEven.oddOrEven(new Scanner(System.in).nextInt());
+        PrimeChecker.primeChecker(new Scanner(System.in).nextInt());
     }
 }
