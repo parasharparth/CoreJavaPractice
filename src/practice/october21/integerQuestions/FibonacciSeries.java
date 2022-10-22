cc093bd624793c915deca892d01eb06701aa794a
parasharparth <parthparashar6@gmail.com>
2022-10-22 00:58:12 -0700

    October21-fibonacci

---
+++ src/practice/october21/integerQuestions/FibonacciSeries.java
@@ -0,0 +1,19 @@
+package practice.october21.integerQuestions;
+
+public class FibonacciSeries {
+
+    public static void fib(int num)
+    {
+        int num1 = 0;
+        int num2 = 1;
+        int count = 0;
+        while(count < num)
+        {
+            System.out.println(num1);
+            int num3 = num1 + num2;
+            num1 = num2;
+            num2 = num3;
+            count = count + 1;
+        }
+    }
+}
