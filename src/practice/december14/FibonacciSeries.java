f20c057e3bfb02299769cd03b6f7093da38edfdd
parasharparth <parthparashar6@gmail.com>
2022-12-16 02:11:32 -0800

    december-15

---
+++ src/practice/december14/FibonacciSeries.java
@@ -0,0 +1,15 @@
+package practice.december14;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the number of terms you want to print for fibonacci series");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        
+    }
+}
