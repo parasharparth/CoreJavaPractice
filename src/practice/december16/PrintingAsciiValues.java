bc5f68b8e2ac13bd584860db74ec01abdab604d5
parasharparth <parthparashar6@gmail.com>
2022-12-16 17:19:16 -0800

    december-16-ascii-value

---
+++ src/practice/december16/PrintingAsciiValues.java
@@ -0,0 +1,14 @@
+package practice.december16;
+
+public class PrintingAsciiValues {
+
+    public static void main(String[] args)
+    {
+        char c1 = 'a';
+        char c2 = 'b';
+        int value1 = c1;
+        int value2 = c2;
+        System.out.println(value1);
+        System.out.println(value2);
+    }
+}
