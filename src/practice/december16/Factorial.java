7578111f2060096543345af675f7b7fc557faa2f
parasharparth <parthparashar6@gmail.com>
2022-12-16 17:17:29 -0800

    december-16-factorial

---
+++ src/practice/december16/Factorial.java
@@ -0,0 +1,25 @@
+package practice.december16;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the number you want the factorial of");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+        if(num == 0)
+        {
+            System.out.println("0! : "+ 1);
+            return;
+        }
+        int fact=1;
+        for(int i=1; i<=num; i++)
+        {
+            fact = fact * i;
+        }
+        System.out.println(num+"! --> " + fact);
+    }
+}
