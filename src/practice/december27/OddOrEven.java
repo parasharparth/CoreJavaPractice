5355ede319c2e1c283b6787e692df59959494dca
parasharparth <parthparashar6@gmail.com>
2022-12-27 23:52:43 -0800

    december-27-odd-or-even

---
+++ src/practice/december27/OddOrEven.java
@@ -0,0 +1,26 @@
+package practice.december27;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    public static void main(String[] args)
+    {
+        while(true) {
+            System.out.println("Enter the number: ");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            sc.close();
+            if (n == 0 || n == 1) {
+                System.out.println("Neither Even nor odd");
+                return;
+            } else if (n % 2 == 0) {
+                System.out.println("Even");
+                return;
+            } else {
+                System.out.println("Odd");
+                return;
+            }
+        }
+    }
+}
