a1c36cc0928132e410e7e5f7be57a77c1e9a2e1b
parasharparth <parthparashar6@gmail.com>
2022-12-29 02:58:10 -0800

    december-27-reverse-a-number

---
+++ src/practice/december28/OddOrEven.java
@@ -0,0 +1,14 @@
+package practice.december28;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the number you want to check");
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+
+    }
+}
